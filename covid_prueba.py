a2ca5b9ec3cdaa714edaa87abca8ba8d5656a78b
Godric Aceves <ct6282@CT-6282-M.local>
2020-07-07 17:27:02 -0500

    refactors code into more readable functions

--- covid_prueba.py
+++ covid_prueba.py
@@ -5,31 +5,19 @@ from urllib.request import urlopen
 from socket import timeout
 import pandas as pd
 import numpy as np
+import datetime
 
 #Se extrae el csv de la web
 url = "http://187.191.75.115/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip"
-try:
-    resp = urlopen(url, timeout=10).read()
-    zipfile = ZipFile(BytesIO(resp))
-    extracted_file = zipfile.open(zipfile.namelist()[0])
-    df = pd.read_csv(extracted_file, encoding = "ISO-8859-1")
-    print("Se abrio el link")
-except (ConnectionResetError, timeout) as error:
-    print(error.args)
-    #print("ConnectionResetError or Timeout")
-    print("Se abrira el archivo zip...")
 
-    zipfile = ZipFile('/home/nacho/Documents/coronavirus/rangel/datos_abiertos_covid19.zip', 'r')
-    extracted_file = zipfile.open(zipfile.namelist()[0])
-    df = pd.read_csv(extracted_file, encoding = "ISO-8859-1")
+def filter_only_positive(df):
+    df = df[df.RESULTADO == 1] #En caso de que se quiera filtrar por solo los que dieron positivo
 
-finally:    
-    #df = df[df.RESULTADO == 1] #En caso de que se quiera filtrar por s{olo los que dieron positivo
+def filter_exclude_columns(df):
     #df.drop(['RESULTADO','FECHA_ACTUALIZACION', 'ID_REGISTRO', 'ORIGEN', 'SECTOR', 'ENTIDAD_UM', 'MIGRANTE', 'PAIS_ORIGEN', 'PAIS_NACIONALIDAD'], axis=1, inplace = True)
-    
-    #Se eliminan las columnas innecesarias
-    df.drop(['FECHA_ACTUALIZACION', 'ID_REGISTRO', 'ORIGEN', 'SECTOR', 'MIGRANTE', 'PAIS_ORIGEN', 'PAIS_NACIONALIDAD'], axis=1, inplace = True)
-    #%%
+    df.drop(['FECHA_ACTUALIZACION', 'ID_REGISTRO', 'ORIGEN', 'SECTOR', 'MIGRANTE', 'PAIS_ORIGEN', 'PAIS_NACIONALIDAD'], axis=1, inplace = True) #Se eliminan las columnas innecesarias
+
+def date_preprocessing(df):
     #convierte a tipo fecha fecha_sintoma 
     df['FECHA_SINTOMAS'] = pd.to_datetime(df['FECHA_SINTOMAS'])
     #restar columna FECHA_INGRESO menos FECHA_SINTOMAS y guardar en columna dias_dif
@@ -41,7 +29,8 @@ finally:
     #verificacion
     df['DIAS_DIF_HOSP'][df['DIAS_DIF_HOSP'] < 0]
     #df['DIAS_DIF_HOSP'].astype(int)
-    ################################################
+
+def filter_negative_dates(df):
     #hace una copia ed fecha_def a dias_dif_def
     df['BOOL_DEF'] = df["FECHA_DEF"].copy()
     #CREAR COLUMNA DE NUMERO DE DIAS DE SINTOMAS A FALLECIMIENTO
@@ -59,71 +48,73 @@ finally:
     df.drop(df[df['DIAS_DIF_DEF'] < 0].index, inplace = True)
     #verificacion
     df['DIAS_DIF_DEF'][df['DIAS_DIF_DEF'] < 0]
-    ####################################################
+
+def filter_binary_status(df):
     #columna defuncion cambia a 0 a los no fallecidos
     df['BOOL_DEF'] = df['BOOL_DEF'].replace(['9999-99-99'], 0)
     #columna defuncion cambia a 1 los fallecidos
     df.loc[df["BOOL_DEF"] != 0, ["BOOL_DEF"]] = 1
-    #%%
+
+def filter_unused_dates(df):
     #eliminar columna FECHA_INGRESO, FECHA_SINTOMAS y FECHA_DEF
-    #df.drop(['FECHA_INGRESO', 'FECHA_SINTOMAS', 'FECHA_DEF'], axis=1, inplace = True)
-    
+    df.drop(['FECHA_INGRESO', 'FECHA_SINTOMAS', 'FECHA_DEF'], axis=1, inplace = True)
+
+def change_to_two(df):
     #Se cambian los valores de 97, 98 y 99 a 2. Se escribe 3 veces por columna a modificar debido a unos errores que encontramos, modificaban datos equivocados
-    df.loc[df['INTUBADO'] == 97, ['INTUBADO']] = 2;df.loc[df['INTUBADO'] == 98, ['INTUBADO']] = 2;df.loc[df['INTUBADO'] == 99, ['INTUBADO']] = 2
-    df.loc[df['NEUMONIA'] == 97, ['NEUMONIA']] = 2;df.loc[df['NEUMONIA'] == 98, ['NEUMONIA']] = 2;df.loc[df['NEUMONIA'] == 99, ['NEUMONIA']] = 2
-    df.loc[df['EMBARAZO'] == 97, ['EMBARAZO']] = 2;df.loc[df['EMBARAZO'] == 98, ['EMBARAZO']] = 2;df.loc[df['EMBARAZO'] == 99, ['EMBARAZO']] = 2
-    df.loc[df['HABLA_LENGUA_INDIG'] == 97, ['HABLA_LENGUA_INDIG']] = 2;df.loc[df['HABLA_LENGUA_INDIG'] == 98, ['HABLA_LENGUA_INDIG']] = 2;df.loc[df['HABLA_LENGUA_INDIG'] == 99, ['HABLA_LENGUA_INDIG']] = 2
-    df.loc[df['DIABETES'] == 97, ['DIABETES']] = 2;df.loc[df['DIABETES'] == 98, ['DIABETES']] = 2;df.loc[df['DIABETES'] == 99, ['DIABETES']] = 2
-    df.loc[df['EPOC'] == 97, ['EPOC']] = 2;df.loc[df['EPOC'] == 98, ['EPOC']] = 2;df.loc[df['EPOC'] == 99, ['EPOC']] = 2
-    df.loc[df['ASMA'] == 97, ['ASMA']] = 2;df.loc[df['ASMA'] == 98, ['ASMA']] = 2;df.loc[df['ASMA'] == 99, ['ASMA']] = 2
-    df.loc[df['INMUSUPR'] == 97, ['INMUSUPR']] = 2;df.loc[df['INMUSUPR'] == 98, ['INMUSUPR']] = 2;df.loc[df['INMUSUPR'] == 99, ['INMUSUPR']] = 2
-    df.loc[df['HIPERTENSION'] == 97, ['HIPERTENSION']] = 2;df.loc[df['HIPERTENSION'] == 98, ['HIPERTENSION']] = 2;df.loc[df['HIPERTENSION'] == 99, ['HIPERTENSION']] = 2
-    df.loc[df['OTRA_COM'] == 97, ['OTRA_COM']] = 2;df.loc[df['OTRA_COM'] == 98, ['OTRA_COM']] = 2;df.loc[df['OTRA_COM'] == 99, ['OTRA_COM']] = 2
-    df.loc[df['CARDIOVASCULAR'] == 97, ['CARDIOVASCULAR']] = 2;df.loc[df['CARDIOVASCULAR'] == 98, ['CARDIOVASCULAR']] = 2;df.loc[df['CARDIOVASCULAR'] == 99, ['CARDIOVASCULAR']] = 2
-    df.loc[df['OBESIDAD'] == 97, ['OBESIDAD']] = 2;df.loc[df['OBESIDAD'] == 98, ['OBESIDAD']] = 2;df.loc[df['OBESIDAD'] == 99, ['OBESIDAD']] = 2
-    df.loc[df['RENAL_CRONICA'] == 97, ['RENAL_CRONICA']] = 2;df.loc[df['RENAL_CRONICA'] == 98, ['RENAL_CRONICA']] = 2;df.loc[df['RENAL_CRONICA'] == 99, ['RENAL_CRONICA']] = 2
-    df.loc[df['TABAQUISMO'] == 97, ['TABAQUISMO']] = 2;df.loc[df['TABAQUISMO'] == 98, ['TABAQUISMO']] = 2;df.loc[df['TABAQUISMO'] == 99, ['TABAQUISMO']] = 2
-    df.loc[df['OTRO_CASO'] == 97, ['OTRO_CASO']] = 2;df.loc[df['OTRO_CASO'] == 98, ['OTRO_CASO']] = 2;df.loc[df['OTRO_CASO'] == 99, ['OTRO_CASO']] = 3
-    df.loc[df['UCI'] == 97, ['UCI']] = 2;df.loc[df['UCI'] == 98, ['UCI']] = 2
-    
+    change_to_two_dictionary = ['INTUBADO', 'NEUMONIA', 'EMBARAZO', 'HABLA_LENGUA_INDIG', 'DIABETES', 'EPOC', 'ASMA', 'INMUSUPR', 'HIPERTENSION', 'OTRA_COM', 'CARDIOVASCULAR', 'OBESIDAD', 'RENAL_CRONICA', 'TABAQUISMO', 'OTRO_CASO', 'UCI']
+    for condition in change_to_two_dictionary:
+        df.loc[df[condition] == 97, [condition]] = 2;df.loc[df[condition] == 98, [condition]] = 2;df.loc[df[condition] == 99, [condition]] = 2
+
+def binary_values(df):
     #Se cambian los valores de 1, 2 e incluso 3 a 0 y 1. Se separa para mantener más claro el proceso
     #En SEXO son: 0 - Hombre, 1 - Mujer, 2 - No especificado
     #En OTRO_CASO se cambiaron los datos: 1 - 1, 2|97|98 - 0, 99 - 2
     #En nacionalidad los datos son: 1- 1, 2|99 - 0 
-    df.loc[df['SEXO'] == 2, ['SEXO']] = 0
-    df.loc[df['SEXO'] == 3, ['SEXO']] = 2
-    df.loc[df['INTUBADO'] == 2, ['INTUBADO']] = 0
-    df.loc[df['NEUMONIA'] == 2, ['NEUMONIA']] = 0
-    df.loc[df['EMBARAZO'] == 2, ['EMBARAZO']] = 0
-    df.loc[df['HABLA_LENGUA_INDIG'] == 2, ['HABLA_LENGUA_INDIG']] = 0
-    df.loc[df['DIABETES'] == 2, ['DIABETES']] = 0
-    df.loc[df['EPOC'] == 2, ['EPOC']] = 0
-    df.loc[df['ASMA'] == 2, ['ASMA']] = 0
-    df.loc[df['INMUSUPR'] == 2, ['INMUSUPR']] = 0
-    df.loc[df['HIPERTENSION'] == 2, ['HIPERTENSION']] = 0
-    df.loc[df['OTRA_COM'] == 2, ['OTRA_COM']] = 0
-    df.loc[df['CARDIOVASCULAR'] == 2, ['CARDIOVASCULAR']] = 0
-    df.loc[df['OBESIDAD'] == 2, ['OBESIDAD']] = 0
-    df.loc[df['RENAL_CRONICA'] == 2, ['RENAL_CRONICA']] = 0
-    df.loc[df['TABAQUISMO'] == 2, ['TABAQUISMO']] = 0
-    df.loc[df['OTRO_CASO'] == 2, ['OTRO_CASO']] = 0
-    df.loc[df['OTRO_CASO'] == 99, ['OTRO_CASO']] = 2    
-    df.loc[df['UCI'] == 2, ['UCI']] = 0
+    binary_values_dictionary = ['SEXO', 'INTUBADO', 'NEUMONIA', 'EMBARAZO', 'HABLA_LENGUA_INDIG', 'DIABETES', 'EPOC', 'ASMA', 'INMUSUPR', 'HIPERTENSION', 'OTRA_COM', 'CARDIOVASCULAR', 'OBESIDAD', 'RENAL_CRONICA', 'TABAQUISMO', 'OTRO_CASO', 'UCI', 'NACIONALIDAD']
+    for condition in binary_values_dictionary:
+        df.loc[df[condition] == 2, [condition]] = 0
+        df.loc[df[condition] == 3, [condition]] = 2
+        df.loc[df[condition] == 99, [condition]] = 0
     df.loc[df['TIPO_PACIENTE'] == 1, ['TIPO_PACIENTE']] = 0
     df.loc[df['TIPO_PACIENTE'] == 2, ['TIPO_PACIENTE']] = 1
     df.loc[df['TIPO_PACIENTE'] == 99, ['TIPO_PACIENTE']] = 0
-    df.loc[df['NACIONALIDAD'] == 2, ['NACIONALIDAD']] = 0
-    df.loc[df['NACIONALIDAD'] == 99, ['NACIONALIDAD']] = 0
-    
+
+def filter_pregnant_men(df):
     #eliminar los hombres embarazados
     df.drop(df[(df['SEXO'] ==0) & (df['EMBARAZO'] ==1)].index, inplace = True)
     #verificacion
     df['SEXO'][(df['SEXO'] ==0) & (df['EMBARAZO'] ==1)]
-    
-    #Se imprime el dataframe
+
+def print_df(df):
+#Se imprime el dataframe    
     #print(df)
-    #%%
     df.to_csv('covid_data.csv', index=False)
     print("Se ha generado el archivo .csv")
 
-
+#Se ejecutan las funciones
+try: #Se obtiene el archivo más reciente
+    resp = urlopen(url, timeout=10).read() #Se omite el uso de una función en este segmento para evitar errores con las variables
+    zipfile = ZipFile(BytesIO(resp))
+    extracted_file = zipfile.open(zipfile.namelist()[0])
+    df = pd.read_csv(extracted_file, encoding = "ISO-8859-1")
+    print("Se está descargando la versión más actual del archivo...")
+    _ = df
+except (ConnectionResetError, timeout) as error: #Si se falla al obtener el archivo más reciente se usa el último registro local
+    print(error.args) #Se omite el uso de una función en este segmento para evitar errores con las variables
+    #print("ConnectionResetError or Timeout")
+    print("Conexión fallida, se usará el último archivo local...")
+    zipfile = ZipFile('/home/nacho/Documents/coronavirus/rangel/datos_abiertos_covid19.zip', 'r')
+    extracted_file = zipfile.open(zipfile.namelist()[0])
+    df = pd.read_csv(extracted_file, encoding = "ISO-8859-1")
+    _ = df
+finally:
+    #filter_only_positive(df)
+    filter_exclude_columns(df)
+    date_preprocessing(df)
+    filter_negative_dates(df)
+    filter_binary_status(df)
+    #filter_unused_dates(df)
+    change_to_two(df)
+    binary_values(df)
+    filter_pregnant_men(df)
+    print_df(df)
